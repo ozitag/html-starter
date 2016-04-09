5b54673ec786dd3eb3fd0d8ddc57193ba4dafc4c
Vital Ozierski <ozicoder@gmail.com>
2016-04-09 16:54:17 +0300

    fixes for dist

---
+++ app/static/js/helpers/tabs.js
@@ -0,0 +1,66 @@
+(function () {
+    'use strict';
+
+    function Tabs($elem, options) {
+        var that = this;
+        this.$elem = $elem;
+
+        options = $.extend({
+            tabsSelector: '.js-tabs a',
+            onShow: function () {
+            }
+        }, options);
+
+        console.log(options);
+
+        var $tabButtons = this.$elem.find(options.tabsSelector);
+        if ($tabButtons.length === 0) {
+            return;
+        }
+
+        var tabsData = {};
+        $tabButtons.each(function () {
+            tabsData[$(this).attr('href')] = {
+                button: $(this),
+                content: $($(this).attr('href')),
+                activated: false
+            };
+        });
+
+        this.showTab = function (tabId) {
+            for (var i in tabsData) {
+                if (tabsData.hasOwnProperty(i)) {
+                    tabsData[i].content.hide();
+                }
+            }
+            $tabButtons.removeClass('active');
+
+            if (tabId in tabsData) {
+                tabsData[tabId].content.show();
+                tabsData[tabId].button.addClass('active');
+
+                if (tabsData[tabId].activated === false) {
+                    tabsData[tabId].activated = true;
+                    options.onShow(tabsData[tabId].content);
+                }
+            }
+        };
+
+        this.showTab($tabButtons.first().attr('href'));
+
+        $tabButtons.on('click', function () {
+            that.showTab($(this).attr('href'));
+            return false;
+        });
+    }
+
+    $.fn.tabs = function (options) {
+        $(this).each(function () {
+            new Tabs($(this), options)
+        });
+    };
+
+    $(function () {
+        $('.js-tabs-container').tabs();
+    });
+})();
