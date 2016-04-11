e37e5d69d0265661efcd48780ec04b9e44025da1
Vital Ozierski <ozicoder@gmail.com>
2016-04-11 15:18:13 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -87,7 +87,7 @@ gulp.task('fix_js_src', function () {
                 $('script').each(function () {
                     var src = $(this).attr('src');
                     if (src.substr(0, 5) !== 'http:' && src.substr(0, 6) !== 'https:') {
-                        src = '/' + config.scriptsPath + '/' + src;
+                        src = '../' + config.scriptsPath + '/' + src;
                     }
                     $(this).attr('src', src);
                 });
