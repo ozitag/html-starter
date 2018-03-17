e8b76af34fd54a672f695d62a84ca59c6ba91d13
vvv <gvv_93@mail.ru>
2018-03-17 12:24:16 +0300

    fix critical bug "prepareJs"

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,38 @@
 "use strict";
 
 global.$ = {
-    gulp: require('gulp'),
-    gulpPlugin: require('gulp-load-plugins')(),
-    bs: require('browser-sync'),
-    fs: require('fs'),
-    del: require('del'),
-    tasks: require('./gulp/tasks.js'),
-    config: require('./config/config.json'),
-    merge: require('merge-stream'),
-    argv: require('yargs').argv
+  gulp: require('gulp'),
+  gulpPlugin: require('gulp-load-plugins')(),
+  bs: require('browser-sync'),
+  fs: require('fs'),
+  del: require('del'),
+  tasks: require('./gulp/tasks.js'),
+  config: require('./config/config.json'),
+  merge: require('merge-stream'),
+  argv: require('yargs').argv
 };
 
 if ($.config.criticalCss) {
-    $.critical = require('critical').stream
+  $.critical = require('critical').stream
 }
 
 $.tasks.forEach(taskPath => {
-    require(taskPath)()
+  require(taskPath)()
 });
 
 $.gulp.task('dev', $.gulp.series(
-    $.gulp.parallel('clean'),
-    $.gulp.parallel('hbs', 'styles', 'scripts', 'scripts:libs','hbs', 'svg', 'svgInline', 'pngSprite'),
-    $.gulp.parallel('prepareHtml'),
-    $.gulp.parallel('watch', 'serve')
+  $.gulp.parallel('clean'),
+  $.gulp.parallel('hbs', 'styles', 'scripts', 'scripts:libs', 'hbs', 'svg', 'svgInline', 'pngSprite'),
+  $.gulp.parallel('prepareHtml'),
+  $.gulp.parallel('watch', 'serve')
 ));
 
 $.gulp.task('build', $.gulp.series(
-    $.gulp.parallel('clean'),
-    $.gulp.parallel('styles', 'scripts', 'scripts:libs'),
-    $.gulp.parallel('hbs', 'svg', 'svgInline', 'pngSprite', 'static:fonts', 'static:images'),
-    $.gulp.parallel('prepareHtml'),
-    $.gulp.parallel('dist', 'content', 'copyMetaFiles'),
-    $.gulp.parallel('imagemin:meta', 'imagemin:content', 'prepareJs', 'replaceHtml', 'criticalCss')
+  $.gulp.parallel('clean'),
+  $.gulp.parallel('styles', 'scripts', 'scripts:libs'),
+  $.gulp.parallel('hbs', 'svg', 'svgInline', 'pngSprite', 'static:fonts', 'static:images'),
+  $.gulp.parallel('prepareHtml'),
+  $.gulp.parallel('dist', 'content', 'copyMetaFiles'),
+  $.gulp.parallel('prepareJs'),
+  $.gulp.parallel('imagemin:meta', 'imagemin:content', 'replaceHtml', 'criticalCss')
 ));
