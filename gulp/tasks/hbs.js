ced6efcae5fc378f157e03ec5fba0b2f54001517
vvlad5 <galko3.v@gmail.com>
2019-11-21 21:38:40 +0300

    apply fixes

--- gulp/tasks/hbs.js
+++ gulp/tasks/hbs.js
@@ -1,31 +1,32 @@
 module.exports = () => {
+  const cache = randomIntNum(1, 5000)
+  const options = {
+    ignorePartials: true,
+    batch: [
+      `${$.config.sourcePath}/${$.config.hbsPath}/layouts`,
+      `${$.config.sourcePath}/${$.config.hbsPath}/partials`,
+    ],
+    helpers: {
+      times: function(n, block) {
+        let accum = ''
+        for (let i = 0; i < n; ++i)
+          accum += block.fn(i + 1)
+        return accum
+      },
+      ifCond: function(v1, v2, options) {
+        if (v1 === v2) {
+          return options.fn(this)
+        }
+        return options.inverse(this)
+      },
+    },
+  }
+
   $.gulp.task('hbs', () => {
     const data = JSON.parse(
       $.fs.readFileSync(`${$.config.sourcePath}/${$.config.dbPath}/db.json`),
     )
-    data.cache = randomIntNum(1, 5000)
-
-    const options = {
-      ignorePartials: true,
-      batch: [
-        `${$.config.sourcePath}/${$.config.hbsPath}/layouts`,
-        `${$.config.sourcePath}/${$.config.hbsPath}/partials`,
-      ],
-      helpers: {
-        times: function(n, block) {
-          let accum = ''
-          for (let i = 0; i < n; ++i)
-            accum += block.fn(i + 1)
-          return accum
-        },
-        ifCond: function(v1, v2, options) {
-          if (v1 === v2) {
-            return options.fn(this)
-          }
-          return options.inverse(this)
-        },
-      },
-    }
+    data.cache = cache
 
     return $.gulp.src([
       `${$.config.sourcePath}/${$.config.hbsPath}/**/*.hbs`,
@@ -42,9 +43,9 @@ module.exports = () => {
       .pipe($.bs.reload({ stream: true }),
       )
   })
-}
 
-function randomIntNum(min, max) {
-  let rand = min - 0.5 + Math.random() * (max - min + 1)
-  return Math.round(rand)
+  function randomIntNum(min, max) {
+    let rand = min - 0.5 + Math.random() * (max - min + 1)
+    return Math.round(rand)
+  }
 }
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,31 +1,32 @@
 module.exports = () => {
   $.gulp.task('watch', () => {
-    $.gulp.watch(
-      [
-        $.config.sourcePath +
-        '/' +
-        $.config.stylesPath +
-        '/**/*.{scss, sass, css}',
+    $.gulp.watch([
+        `${$.config.sourcePath}/${$.config.stylesPath}/**/*.{scss, sass, css}`,
       ],
       $.gulp.series('styles'),
     )
-    $.gulp.watch(
-      [$.config.sourcePath + '/' + $.config.hbsPath + '/**/*'],
+    $.gulp.watch([
+        `${$.config.sourcePath}/${$.config.hbsPath}/**/*`,
+        `${$.config.sourcePath}/${$.config.dbPath}/db.json`,
+      ],
       $.gulp.series(['hbs', 'prepareHtmlDev']),
     )
-    $.gulp.watch(
-      [$.config.sourcePath + '/' + $.config.svgPath + '/*.svg'],
+    $.gulp.watch([
+        `${$.config.sourcePath}/${$.config.svgPath}/*.svg`,
+      ],
       $.gulp.series('svg'),
     )
-    $.gulp.watch(
-      [$.config.sourcePath + '/' + $.config.svgInlinePath + '/*.svg'],
+    $.gulp.watch([
+        `${$.config.sourcePath}/${$.config.svgInlinePath}/*.svg`,
+      ],
       $.gulp.series('svgInline'),
     )
     $.gulp.watch([
-      $.config.sourcePath + '/' + $.config.metaPath + '/*.{png,jpg,jpeg}',
+      `${$.config.sourcePath}/${$.config.metaPath}/*.{png,jpg,jpeg}`,
     ])
-    $.gulp.watch(
-      [$.config.sourcePath + '/' + $.config.pngPath + '/*.png'],
+    $.gulp.watch([
+        `${$.config.sourcePath}/${$.config.pngPath}/*.png`,
+      ],
       $.gulp.series('pngSprite'),
     )
   })
