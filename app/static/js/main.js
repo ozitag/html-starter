fc6e5e4206625274b658d2966f057b7fc18222fc
vvlad5 <galko3.v@gmail.com>
2020-01-08 18:52:00 +0300

    fix dropdown

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,5 +1,4 @@
 import layout from './modules/layout';
 import scrollControl from './modules/scroll-control';
 import preloader from './modules/preloader';
-import dropdown from '../../../dev_assets/js/dropdown';
 import ui from './modules/ui';
