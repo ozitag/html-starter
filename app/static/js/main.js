3e4b4b892eb518203091e7ca2feded1b43c52a0c
vvlad5 <galko3.v@gmail.com>
2020-01-24 13:28:58 +0300

    update scrollControl & import observers by default

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,4 +1,6 @@
 import layout from './modules/utils/layout';
+import scrollObserver from './modules/utils/scroll-observer';
+import resizeObserver from './modules/utils/resize-observer';
 import scrollControl from './modules/utils/scroll-control';
 import preloader from './modules/utils/preloader';
 import popups from './modules/utils/popups';
--- app/static/js/modules/utils/scroll-control.js
+++ app/static/js/modules/utils/scroll-control.js
@@ -1,5 +1,19 @@
 class ScrollControl {
-  static show() {
+  constructor () {
+    this.isFixedScroll = false;
+    this.lastScrollPos = this._getScrollPos();
+
+    listenScroll(() => {
+      if (this.isFixedScroll) return false;
+      this.lastScrollPos = this._getScrollPos();
+    });
+  }
+
+  _getScrollPos() {
+    return window.pageYOffset;
+  }
+
+  showScrollbar () {
     if (!document.body.classList.contains('fixed-scroll')) {
       return false;
     }
@@ -8,26 +22,38 @@ class ScrollControl {
     document.body.style.paddingRight = ``;
 
     if (isMobileLayout()) {
-      const currYOffset = getComputedStyle(document.body).top;
+      this.lastScrollPos = parseFloat(getComputedStyle(document.body).top || '0');
       document.body.style.top = ``;
-      window.scrollTo(0, parseFloat(currYOffset || '0') * -1);
+      window.scrollTo(0, this.lastScrollPos * -1);
     }
+
+    this.isFixedScroll = false;
   }
 
-  static hide() {
+  hideScrollbar () {
     if (document.body.classList.contains('fixed-scroll')) {
       return false;
     }
 
     if (isMobileLayout()) {
-      document.body.style.top = `-${window.pageYOffset}px`;
+      document.body.style.top = `-${this.lastScrollPos}px`;
     }
 
     document.body.classList.add('fixed-scroll');
     document.body.style.paddingRight = ScrollControl._calcScrollbarWidth();
+
+    this.isFixedScroll = true;
   }
 
-  static _calcScrollbarWidth() {
+  getScrollbarState () {
+    return this.isFixedScroll;
+  }
+
+  getLastScrollPos () {
+    return this.lastScrollPos;
+  }
+
+  static _calcScrollbarWidth () {
     const scrollbarMeasure = document.createElement('div');
     scrollbarMeasure.className = 'scroll-measure';
 
@@ -43,5 +69,9 @@ class ScrollControl {
   }
 }
 
-window.showScrollbar = ScrollControl.show;
-window.hideScrollbar = ScrollControl.hide;
+const scrollControl = new ScrollControl();
+
+window.showScrollbar = scrollControl.showScrollbar.bind(scrollControl);
+window.hideScrollbar = scrollControl.hideScrollbar.bind(scrollControl);
+window.getLastScrollPos = scrollControl.getLastScrollPos.bind(scrollControl);
+window.isFixedSCroll = scrollControl.getScrollbarState.bind(scrollControl);
