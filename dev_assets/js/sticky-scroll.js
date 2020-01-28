4410459e8ec516056a14006b2099f46787df47a5
vvlad5 <galko3.v@gmail.com>
2020-01-28 13:41:02 +0300

    improve scicky-scroll

--- dev_assets/js/sticky-scroll.js
+++ dev_assets/js/sticky-scroll.js
@@ -1,65 +1,81 @@
-class StickyScroll {
-  constructor (elem) {
-    this.elem = elem;
-    this.parent = elem.parentNode;
-    this.options = {
-      fixedOffset: 0,
-    };
+;(function(global) {
+  'use strict';
 
-    raf(() => this.checkPos());
-    this.addEvents();
-  }
+  class StickyProduct {
+    constructor (elem) {
+      this.elem = elem;
+      this.elemHeight = StickyProduct.getHeight(elem);
+      this.parent = elem.parentNode;
+      this.parentHeight = StickyProduct.getHeight(this.parent);
+      this.options = {
+        fixedOffset: 30,
+      };
 
-  addEvents () {
-    listenScroll(() => this.checkPos());
-    Layout.addListener(() => this.checkPos());
-  }
+      raf(() => this.checkPos());
+      this.addEvents();
+    }
 
-  checkPos () {
-    if (!isTabletLayout()) {
-      const parentCordTop = this.parent.getBoundingClientRect().top;
-      const breakpointTop = parentCordTop - this.options.fixedOffset;
+    addEvents () {
+      listenScroll(() => this.checkPos());
+      listenResize(() => {
+        this.elemHeight = StickyProduct.getHeight(this.elem);
+        this.parentHeight = StickyProduct.getHeight(this.parent);
+      });
+      Layout.addListener(() => this.checkPos());
+    }
 
-      if (breakpointTop < this.options.fixedOffset) {
-        const itemOffset = this.parent.offsetHeight - this.elem.offsetHeight;
-        const breakpointBottom = breakpointTop + itemOffset;
+    checkPos () {
+      if (!isTabletLayout()) {
+        const parentCordTop = this.parent.getBoundingClientRect().top;
+        const breakpointTop = parentCordTop - this.options.fixedOffset;
 
-        if (breakpointBottom < 0) {
-          this.changePos('bottom', itemOffset);
+        if (breakpointTop < 0) {
+          const itemOffset = this.parentHeight - this.elemHeight;
+          const breakpointBottom = breakpointTop + itemOffset;
+
+          if (breakpointBottom < 0) {
+            this.changePos('bottom', itemOffset);
+          } else {
+            this.changePos('fix');
+          }
         } else {
-          this.changePos('fix');
+          this.changePos('top');
         }
       } else {
         this.changePos('top');
       }
     }
-  }
 
-  changePos (pos, itemOffset) {
-    switch (pos) {
-      case 'fix':
-        this.elem.classList.add('fixed');
-        this.elem.style.transform = `translate3d(0, ${this.options.fixedOffset}px, 0)`;
-        break;
-      case 'top':
-        this.elem.classList.remove('fixed');
-        this.elem.style.transform = ``;
-        break;
-      case 'bottom':
-        this.elem.classList.remove('fixed');
-        this.elem.style.transform = `translate3d(0, ${itemOffset}px, 0)`;
+    changePos (pos, itemOffset) {
+      switch (pos) {
+        case 'fix':
+          this.elem.classList.add('fixed');
+          this.elem.style.transform = `translate3d(0, ${this.options.fixedOffset}px, 0)`;
+          break;
+        case 'top':
+          this.elem.classList.remove('fixed');
+          this.elem.style.transform = ``;
+          break;
+        case 'bottom':
+          this.elem.classList.remove('fixed');
+          this.elem.style.transform = `translate3d(0, ${itemOffset}px, 0)`;
+      }
+    }
+
+    static getHeight (elem) {
+      return elem.offsetHeight;
+    }
+
+    static init () {
+      const elems = document.querySelectorAll('[data-pos="sticky"]');
+      if (!elems.length) return false;
+      elems.forEach(elem => {
+        if ('init' in elem.dataset) return false;
+        elem.setAttribute('data-init', true);
+        return new StickyProduct(elem);
+      });
     }
   }
 
-  static init () {
-    const elems = document.querySelectorAll('[data-scroll="sticky"]');
-    if (!elems.length) return false;
-    elems.forEach(elem => {
-      if ('init' in elem.dataset) return false;
-      elem.setAttribute('data-init', true);
-      return new StickyScroll(elem);
-    });
-  }
-}
-
-window.StickyScroll = StickyScroll;
+  global.StickyProduct = StickyProduct;
+})(window);
