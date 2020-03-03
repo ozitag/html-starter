81135a7b998a3687a441ba9726a4876a4d577132
vgalko <v.galko@ozitag.com>
2020-03-03 21:11:15 +0300

    implement share.js & apply fixes

--- dev_assets/js/share.js
+++ dev_assets/js/share.js
@@ -1,6 +1,6 @@
-var Share = {
-  vkontakte: function(purl, ptitle, pimg, text) {
-    var url = 'http://vkontakte.ru/share.php?';
+const Share = {
+  vk: (purl, ptitle, pimg, text) => {
+    let url = 'http://vkontakte.ru/share.php?';
     url += 'url=' + encodeURIComponent(purl);
     url += '&title=' + encodeURIComponent(ptitle);
     url += '&description=' + encodeURIComponent(text);
@@ -8,53 +8,60 @@ var Share = {
     url += '&noparse=true';
     Share.popup(url);
   },
-  odnoklassniki: function(purl) {
-    var url = 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1';
-    url += '&st._surl=' + purl;
+  ok: (purl, ptitle, pimg, text) => {
+    let url = 'https://connect.ok.ru/offer?';
+    url += 'url=' + encodeURIComponent(purl);
+    url += '&title=' + encodeURIComponent(ptitle);
+    url += '&description=' + encodeURIComponent(text);
+    url += '&imageUrl=' + encodeURIComponent(pimg);
     Share.popup(url);
   },
-  facebook: function(purl, ptitle, pimg, text) {
-    var url = 'http://www.facebook.com/sharer.php?s=100';
+  fb: (purl, ptitle, pimg, text) => {
+    let url = 'http://www.facebook.com/sharer.php?s=100';
     url += '&p[title]=' + encodeURIComponent(ptitle);
     url += '&p[summary]=' + encodeURIComponent(text);
     url += '&p[url]=' + encodeURIComponent(purl);
     url += '&p[images][0]=' + encodeURIComponent(pimg);
     Share.popup('https://www.facebook.com/sharer/sharer.php?u=' + purl);
   },
-
-  twitter: function(purl, ptitle, pimg, text) {
-    var url = 'http://twitter.com/share?';
-    if (typeof ptitle !== 'undefined') url += 'text=' + encodeURIComponent(ptitle.length ? ptitle : text);
+  tw: (purl, ptitle, pimg, text) => {
+    let url = 'http://twitter.com/share?';
+    if (typeof ptitle !== 'undefined') {
+      url += 'text=' + encodeURIComponent(ptitle.length ? ptitle : text);
+    }
     url += '&url=' + encodeURIComponent(purl);
     url += '&counturl=' + encodeURIComponent(purl);
     Share.popup(url);
   },
-
-  gplus: function(purl) {
-    Share.popup('https://plus.google.com/share?url=' + encodeURIComponent(purl));
-  },
-
-  popup: function(url) {
+  popup: url => {
     window.open(url, '', 'toolbar=0,status=0,width=626,height=436');
   },
 };
 
-function getOgParam(param) {
-  var $elem = $('meta[property="og:' + param + '"]');
-  return $elem.length ? $elem.attr('content') : '';
+function getOgParam (param) {
+  const elem = document.querySelector('meta[property="og:' + param + '"]');
+  return elem.content || '';
 }
 
-$(function() {
-  $('.js-social-share button').on('click', function() {
-    var social = $(this).data('social');
-    var func = social in Share ? Share[social] : null;
+function initShare () {
+  const buttons = document.querySelectorAll('.js-social-share [data-social]');
+  buttons.forEach(button => {
+    button.addEventListener('click', e => {
+      e.preventDefault();
+      const { currentTarget } = e;
+      const social = currentTarget.dataset.social;
+      const handler =  Share[social] || null;
 
-    if (func === null) {
-      return false;
-    }
+      if (handler === null) return false;
 
-    func(getOgParam('url'), getOgParam('title'), getOgParam('image'), getOgParam('description'));
-
-    return false;
+      handler(
+        getOgParam('url'),
+        getOgParam('title'),
+        getOgParam('description'),
+        getOgParam('image'),
+      );
+    });
   });
-});
+}
+
+window.initShare = initShare;
