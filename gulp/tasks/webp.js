2ce406700b161a2df0e159d6785ea27485b00a3a
Vital Ozierski <v.ozierski@ozitag.com>
2020-08-17 18:18:45 +0300

    set quality to 100 for webp images

--- gulp/tasks/webp.js
+++ gulp/tasks/webp.js
@@ -3,6 +3,6 @@ module.exports = () => {
     if (!$.config.buildWebp) return false;
     return $.gulp.src([
       `${$.config.sourcePath}/${$.config.assetsPath}/images/**/*`,
-    ]).pipe($.webp()).pipe($.gulp.dest(`${$.config.destPath}/${$.config.assetsPath}/images`));
+    ]).pipe($.webp({quality: 100})).pipe($.gulp.dest(`${$.config.destPath}/${$.config.assetsPath}/images`));
   });
 };
