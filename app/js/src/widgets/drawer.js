8163cc435e65ec024ee35a138ffa4e804ee56f93
seryozha.baleyko <seryozha.baleyko@gmail.com>
2020-12-05 23:54:40 +0300

    fix: drawer

--- app/js/src/widgets/drawer.js
+++ app/js/src/widgets/drawer.js
@@ -1,38 +1,6 @@
-class Store {
-    constructor(name, defaultValue = [], options = {}, callback) {
-        callback = callback || function () { };
-
-        this._dbName = name;
-
-        if (!localStorage.getItem(name)) {
-            localStorage.setItem(name, JSON.stringify(defaultValue));
-        }
-
-        callback.call(this, JSON.parse(localStorage.getItem(name)));
-
-        const defaults = {
-            prefix: '',
-        };
-
-        this.config = { ...defaults, ...options };
-    }
-
-    __getPrefixedKey(key, options = {}) {
-        if (options.noPrefix) {
-            return key;
-        } else {
-            return this.config.prefix + key;
-        }
-    }
-}
-
 const CLASS_NAME_DRAWER = 'js-drawer';
 const CLASS_NAME_DRAWER_EMBED_CONTAINER = 'js-drawer-embed-container';
 const CLASS_NAME_DRAWER_CONTAINER = 'js-drawer-container';
-const CLASS_NAME_DRAWER_NAV = 'js-drawer-nav';
-const CLASS_NAME_DRAWER_NAV_ITEM = 'js-drawer-nav-item';
-const CLASS_NAME_DRAWER_CONTENT = 'js-drawer-content';
-const CLASS_NAME_DRAWER_CONTENT_ITEM = 'js-drawer-content-item';
 const CLASS_NAME_ACTIVE = 'active';
 const CLASS_NAME_OPEN = 'open';
 const CLASS_NAME_CLOSED = 'closed';
@@ -40,10 +8,6 @@ const CLASS_NAME_CLOSED = 'closed';
 const SELECTOR_DRAWER = `.${CLASS_NAME_DRAWER}`;
 const SELECTOR_DRAWER_EMBED_CONTAINER = `.${CLASS_NAME_DRAWER_EMBED_CONTAINER}`;
 const SELECTOR_DRAWER_CONTAINER = `.${CLASS_NAME_DRAWER_CONTAINER}`;
-const SELECTOR_DRAWER_NAV = `.${CLASS_NAME_DRAWER_NAV}`;
-const SELECTOR_DRAWER_NAV_ITEM = `.${CLASS_NAME_DRAWER_NAV_ITEM}`;
-const SELECTOR_DRAWER_CONTENT = `.${CLASS_NAME_DRAWER_CONTENT}`;
-const SELECTOR_DRAWER_CONTENT_ITEM = `.${CLASS_NAME_DRAWER_CONTENT_ITEM}`;
 
 const defaults = {
     position: 'left',
@@ -69,7 +33,6 @@ const defaults = {
 class Drawer {
     constructor(target, options) {
         this.config = {...defaults, ...options};
-        this.store = new Store(this.config.nameOfStore);
 
         if (!target) {
             throw new Error('Missing required attribute: target element (css selector or DOM node) must be provided');
@@ -85,8 +48,6 @@ class Drawer {
             this.targetElement.parentElement.classList.add(CLASS_NAME_DRAWER_EMBED_CONTAINER);
         }
         this.containerElement = this.targetElement.querySelector(SELECTOR_DRAWER_CONTAINER);
-        this.contentElement = this.targetElement.querySelector(SELECTOR_DRAWER_CONTENT);
-        this.navElement = this.targetElement.querySelector(SELECTOR_DRAWER_NAV);
         this.__applyConfig(this.config);
         this.__attachEventListeners();
     }
@@ -97,32 +58,10 @@ class Drawer {
 
     setHeight(height) {
         this.targetElement.style.height = height;
-        if (this.config.position === 'top' || this.config.position === 'bottom') {
-            this.containerElement.style.height = `calc(100% - ${this.navElement.clientHeight}px)`;
-        }
     }
 
     setWidth(width) {
         this.targetElement.style.width = width;
-        if (this.config.position === 'left' || this.config.position === 'right') {
-            this.containerElement.style.width = `calc(100% - ${this.navElement.clientWidth}px)`;
-        }
-    }
-
-    setNavigationSize(width, height) {
-        if (this.config.position === 'left' || this.config.position === 'right') {
-            this.navElement.style.width = width;
-        }
-        if (this.config.position === 'top' || this.config.position === 'bottom') {
-            this.navElement.style.height = height;
-        }
-        const navItems = this.navElement.querySelectorAll(SELECTOR_DRAWER_NAV_ITEM);
-        let l = navItems.length;
-
-        while (l--) {
-            navItems[l].style.width = width;
-            navItems[l].style.height = height;
-        }
     }
 
     setAutoClose(delay) {
@@ -130,36 +69,6 @@ class Drawer {
             clearTimeout(this.autocloseTimeout);
         }
         this.autocloseTimeout = setTimeout(this.close.bind(this), delay);
-    }
-
-    setActiveById(panel) {
-        let id;
-        const previousActivePanel = this.targetElement.querySelector(`${SELECTOR_DRAWER_CONTENT_ITEM}.${CLASS_NAME_ACTIVE}`);
-        if (this.__isDOMElement(panel)) {
-            id = panel.id;
-        } else if (this.__isString(panel)) {
-            id = panel;
-        } else {
-            return;
-        }
-
-        if (previousActivePanel && id === previousActivePanel.id) {
-            return;
-        }
-
-        this.fire('beforeToggle', [previousActivePanel, currentActivePanel]);
-        const activeNavButton = this.targetElement.querySelector(`${SELECTOR_DRAWER_NAV_ITEM}[data-drawer-target="${id}"]`);
-        const currentActivePanel = id ? this.targetElement.querySelector(`#${id}`) : null;
-
-        this.__deactivateAll();
-        currentActivePanel && currentActivePanel.classList.add(CLASS_NAME_ACTIVE);
-        activeNavButton && activeNavButton.classList.add(CLASS_NAME_ACTIVE);
-        this.fire('afterToggle', [previousActivePanel, currentActivePanel]);
-    }
-
-    setActiveByIndex(index) {
-        const allPanels = this.contentElement && this.contentElement.children;
-        this.setActiveById(allPanels[index] && allPanels[index].id);
     }
 
     open() {
@@ -171,6 +80,9 @@ class Drawer {
         this.targetElement.classList.add(CLASS_NAME_OPEN);
         this.containerElement.style.left = '';
         this.__updateEmbeding();
+        if (window.localStorage) {
+            window.localStorage.setItem(this.config.nameOfStore, JSON.stringify(true));
+        }
         this.fire('afterOpen', [this.targetElement]);
     }
 
@@ -183,6 +95,9 @@ class Drawer {
         this.targetElement.classList.add(CLASS_NAME_CLOSED);
         this.containerElement.style.left = `${this.config.minClosedSize}px`;
         this.__updateEmbeding();
+        if (window.localStorage) {
+            window.localStorage.setItem(this.config.nameOfStore, JSON.stringify(false));    
+        }
         this.fire('afterClose', [this.targetElement]);
     }
 
@@ -204,14 +119,9 @@ class Drawer {
     __isStart(value) {
         if (this.__isBoolean(value)) {
             config.startOpen ? this.open() : this.close();
-        } else if (this.__isString(value)) {
-            if (value === 'save') {
-                console.log('save');
-                this.close()
-
-                if (this.store) {
-
-                }
+        } else if (this.__isString(value) && value === 'save' && window.localStorage && window.localStorage.getItem(this.config.nameOfStore) !== null) {
+            if (JSON.parse(window.localStorage.getItem(this.config.nameOfStore)) === true) {
+                this.open();
             } else {
                 this.close();
             }
@@ -222,31 +132,16 @@ class Drawer {
 
     __applyConfig(config) {
         this.setPosition(config.position);
-        // config.startOpen ? this.open() : this.close();
         this.__isStart(config.startOpen);
-        this.setNavigationSize(
-            config.navigationItemWidth,
-            config.navigationItemHeight
-        );
-
         this.setWidth(config.width);
         this.setHeight(config.height);
         this.__updateEmbeding();
         if (config.autoclose) {
             this.setAutoClose();
         }
-        this.__setAnimations(config.animations);
-        this.setActiveById(this.config.activePanel || this.__getContentPanel(0));
     }
 
     __attachEventListeners() {
-        const navButtons = this.navElement.children;
-        let l = navButtons.length;
-
-        while (l--) {
-            navButtons[l].onclick = this.onNavButtonClick.bind(this);
-        }
-
         if (this.autocloseTimeout) {
             this.containerElement.addEventListener('mouseover', this.onMouseOver.bind(this));
             this.containerElement.addEventListener('mouseout', this.onMouseOut.bind(this));
@@ -287,42 +182,12 @@ class Drawer {
         }
     }
 
-    __setAnimations(animations) {
-        if (!animations || !Object.keys(animations).length) {
-            return;
-        }
-
-        if (animations.replace) {
-            const animation = this.__getAnimationFromString(animations.replace);
-            const allContentPanels = this.contentElement.children;
-            let l = allContentPanels.length;
-            while (l--) {
-                this.__setVendorStyleProperty(
-                    allContentPanels[l],
-                    'transition',
-                    this.__getTransitionFromAnimation(animation)
-                );
-                allContentPanels[l].classList.add(animation.type);
-            }
-        }
-        if (animations.toggle) {
-            const animation = this.__getAnimationFromString(animations.toggle);
-            this.__setVendorStyleProperty(
-                this.targetElement,
-                'transition',
-                this.__getTransitionFromAnimation(animation)
-            );
-        }
-    }
-
     onNavButtonClick(event) {
         if (!this.isOpen()) {
             this.open();
         } else if (this.isActive(event.currentTarget) && this.config.closeable) {
             this.close();
         }
-
-        this.setActiveById(event.currentTarget.getAttribute('data-drawer-target'));
     }
 
     onMouseOver() {
@@ -358,23 +223,6 @@ class Drawer {
             return;
         }
         this.config.listeners[eventName].apply(this, args);
-    }
-
-    __deactivateAll() {
-        const allPanels = this.contentElement && this.contentElement.children;
-        const allNavButtons = this.navElement && this.navElement.children;
-        let l = allPanels.length;
-        let ll = allNavButtons.length;
-        while (l--) {
-            allPanels[l].classList.remove(CLASS_NAME_ACTIVE);
-        }
-        while (ll--) {
-            allNavButtons[ll].classList.remove(CLASS_NAME_ACTIVE);
-        }
-    }
-
-    __getContentPanel(index) {
-        return this.contentElement && this.contentElement.children && this.contentElement.children[index];
     }
 
     __isDOMElement(target) {
