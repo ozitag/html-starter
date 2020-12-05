55b384c909db0322acad64a76d46814585d73817
Vital Ozierski <v.ozierski@ozitag.com>
2020-12-05 16:03:24 +0300

    add helper 'isClickOutsideElement'

--- app/js/src/utils/helpers.js
+++ app/js/src/utils/helpers.js
@@ -14,15 +14,6 @@ function isLocalhost() {
   return document.location.href.indexOf('localhost') !== -1 || document.location.href.indexOf('192.168') !== -1;
 }
 
-function triggerInputChange(element) {
-  if ('createEvent' in document) {
-    var evt = document.createEvent('HTMLEvents');
-    evt.initEvent('change', false, true);
-    element.dispatchEvent(evt);
-  } else
-    element.fireEvent('onchange');
-}
 
 window.isLocalhost = isLocalhost;
 window.buildThresholdList = buildThresholdList;
-window.triggerInputChange = triggerInputChange;
