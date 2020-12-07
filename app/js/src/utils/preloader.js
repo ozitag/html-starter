492274fd5e3658bd1213fe8ef171d08ab0e5468f
seryozha.baleyko <seryozha.baleyko@gmail.com>
2020-12-07 23:24:16 +0300

    add: added prealoader for uikit

--- app/js/src/utils/preloader.js
+++ app/js/src/utils/preloader.js
@@ -1,21 +1,46 @@
 class Preloader {
   static disablingPreloader() {
     const preloader = document.querySelector('.js-preloader');
-    const handler = e => {
-      const { target, currentTarget } = e;
-      if (target !== currentTarget) return false;
-      preloader.removeEventListener(endEvents.animation, handler);
-      preloader.style.display = 'none';
-      preloader.classList.remove('hide');
+    
+    if (preloader) {
+      const handler = e => {
+        const { target, currentTarget } = e;
+        if (target !== currentTarget) return false;
+        preloader.removeEventListener(endEvents.animation, handler);
+        preloader.style.display = 'none';
+        preloader.classList.remove('hide');
 
+        setTimeout(() => {
+          const pageEvent = new CustomEvent('pageLoaded');
+          document.dispatchEvent(pageEvent);
+        }, 1000);
+      };
+      preloader.addEventListener(endEvents.animation, handler);
+
+      preloader.classList.add('hide');
+    }
+
+    const uikitPreloader = document.querySelector('.js-uikit-preloader');
+
+    if (uikitPreloader) {
+      const handler = e => {
+        const { target, currentTarget } = e;
+        if (target !== currentTarget) return false;
+        uikitPreloader.removeEventListener(endEvents.animation, handler);
+        uikitPreloader.style.display = 'none';
+        uikitPreloader.classList.remove('hide');
+
+        setTimeout(() => {
+          const pageEvent = new CustomEvent('pageLoaded');
+          document.dispatchEvent(pageEvent);
+        }, 1000);
+      };
       setTimeout(() => {
-        const pageEvent = new CustomEvent('pageLoaded');
-        document.dispatchEvent(pageEvent);
-      }, 1000);
-    };
-    preloader.addEventListener(endEvents.animation, handler);
-
-    preloader.classList.add('hide');
+        uikitPreloader.addEventListener(endEvents.animation, handler);
+        uikitPreloader.classList.add('hide');
+      }, 500);
+    }
+    
     document.body.classList.add('loaded');
   }
 }
