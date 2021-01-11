fcf254dca9946c29d1e5776eecfaebcfdebff65a
Vital Ozierski <v.ozierski@ozitag.com>
2021-01-11 22:08:54 +0300

    fix scroll up when checkbox, radio selected

--- gulp/tasks/hbs.js
+++ gulp/tasks/hbs.js
@@ -16,6 +16,32 @@ module.exports = () => {
 
         return accum;
       },
+      when: function(v1, operator, v2, options) {
+        switch (operator) {
+          case '==':
+            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+          case '===':
+            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+          case '!=':
+            return (v1 != v2) ? options.fn(this) : options.inverse(this);
+          case '!==':
+            return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+          case '<':
+            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+          case '<=':
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+          case '>':
+            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+          case '>=':
+            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+          case '&&':
+            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+          case '||':
+            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+          default:
+            return options.inverse(this);
+        }
+      },
       ifCond: function(v1, v2, options) {
         if (v1 === v2) return options.fn(this);
 
